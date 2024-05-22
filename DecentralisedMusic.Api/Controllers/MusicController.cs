d82ef6ea18ab46c8c6dfab49d3521f4af006ab55
Alex Connolly <alex.connolly.webmaster@gmail.com>
2024-05-22 12:40:15 +0100

    test example file

---
+++ DecentralisedMusic.Api/Controllers/MusicController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DecentralisedMusic.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicController : ControllerBase
+    {
+        [HttpGet("{trackId}")]
+        public IActionResult StreamTrack(string trackId)
+        {
+            var filePath = Path.Combine("Files", trackId + ".mp3");
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+
+            var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+            return File(stream, "audio/mpeg", enableRangeProcessing: true);
+        }
+    }
+}
